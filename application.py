c3bdea7f8bcded8434246de122b379dcab962c96
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2024-02-08 10:49:49 +0100

    Update application.py

--- application.py
+++ application.py
@@ -11,76 +11,7 @@ application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR
 
 db = SQLAlchemy(application) 
 
-class ToDoModel(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    task = db.Column(db.String(200))
-    summary = db.Column(db.String(500))
 
-task_post_args = reqparse.RequestParser()
-task_post_args.add_argument("task", type=str, help="Task is required.", required=True)
-task_post_args.add_argument("summary", type=str, help="Summary is required", required=True)
-
-task_put_args = reqparse.RequestParser()
-task_put_args.add_argument("task", type=str)
-task_put_args.add_argument("summary", type=str)
-
-resource_fields = {
-    'id': fields.Integer,
-    'task': fields.String,
-    'summary': fields.String
-}
-
-class Home(Resource):
-    def get(self):
-        return "Welcome"
-
-class ToDoList(Resource):
-    def get(self):
-        tasks = ToDoModel.query.all()
-        todos = {}
-        for task in tasks:
-            todos[task.id] = {"task": task.task, "summary": task.summary}
-        return todos
-
-class ToDo(Resource):
-    @marshal_with(resource_fields)
-    def get(self, todo_id):
-        task = ToDoModel.query.filter_by(id=todo_id).first()
-        if not task:
-            abort(404, message="Could not find task with that id")
-        return task
-    
-    @marshal_with(resource_fields)
-    def post(self, todo_id):
-        args = task_post_args.parse_args()
-        task = ToDoModel.query.filter_by(id=todo_id).first()
-        if task:
-            abort(409, "Task ID already taken")
-        todo = ToDoModel(id=todo_id, task=args['task'], summary=args['summary'])
-        db.session.add(todo)
-        db.session.commit()
-        return todo[todo_id]
-    
-    @marshal_with(resource_fields)
-    def put(self, todo_id):
-        args = task_put_args.parse_args()
-        task = ToDoModel.query.filter_by(id=todo_id).first()
-        if not task:
-            abort(404, message="task doesn't exist, cannot update")
-        if args['task']:
-            task.task = args['task']
-        if args['summary']:
-            task.summary = args['summary']
-        db.session.commit()
-        return task
-
-    def delete(self, todo_id):
-        task = ToDoModel.query.filter_by(id=todo_id).first()
-        db.session.delete(task)
-        return 'Todo Deleted', 204
-
-api.add_resource(ToDo, '/todos/<int:todo_id>')
-api.add_resource(ToDoList, '/todos')
 api.add_resource(Home, '/')
 
 if __name__ == '__main__':
