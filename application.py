6d00133581c7421c74704a87eef145cdc909a932
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2024-02-08 10:52:09 +0100

    Update application.py

--- application.py
+++ application.py
@@ -5,13 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 application = Flask(__name__)
 api = Api(application)
 
-DBVAR = f"postgresql://{os.environ['RDS_USERNAME']}:{os.environ['RDS_PASSWORD']}@{os.environ['RDS_HOSTNAME']}/{os.environ['RDS_DB_NAME']}"
-application.config['SECRET_KEY'] = '1dfc4dedcdsdsd5b2ffa3a090dfc34f845fd'
-application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR 
-
-db = SQLAlchemy(application) 
-
-
 api.add_resource(Home, '/')
 
 if __name__ == '__main__':
